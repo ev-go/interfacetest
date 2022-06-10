78b1872b017af4cb481e1dcc6be5e05c2fe611f3
ev <dmitry_dtv@mail.ru>
2022-06-10 22:04:23 +0500

    220610
    way to move

--- main.go
+++ main.go
@@ -1,44 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
-type Man struct{}
-
-func (m Man) Sleep() {}
-func (m Man) Eat()   {}
-func (m Man) Work()  {}
-
-// и тип Пёс, который у нас умеет только лаять
-type Dog struct{}
-
-func (d Dog) Bark() {}
-
-// теперь мы хотим понять, кто может стать программистом,
-// для этого мы определяем интерфейс Programmer,
-// и определяем в нём метод Work
-// этим мы ограничиваем количество объектов,
-// которые смогут быть программистами
-// так случилось, что в нашем коде, чтобы стать программистом,
-// достаточно уметь работать :)
-type Programmer interface {
-	Work()
+type WayToMove interface {
+	move()
 }
 
-type request interface {
+// структура "Автомобиль"
+type Car struct{}
+
+// структура "Самолет"
+type Aircraft struct{}
+
+type Boat struct{}
+
+func (c Car) move() {
+	fmt.Println("Автомобиль едет")
+}
+func (a Aircraft) move() {
+	fmt.Println("Самолет летит")
+}
+func (a Boat) move() {
+	fmt.Println("Лодка идет")
 }
 
 func main() {
-	Vasiliy := Man{}
-	fmt.Printf("%s\n", reflect.TypeOf(Vasiliy).String())
 
-	Sharik := Dog{}
-	fmt.Printf("%s\n", reflect.TypeOf(Sharik).String())
-
-	var worker Programmer
-	worker = Vasiliy
-
-	fmt.Printf("%s\n", reflect.TypeOf(worker).String())
+	var tesla WayToMove = Car{}
+	var boing WayToMove = Aircraft{}
+	var admiral WayToMove = Boat{}
+	tesla.move()
+	boing.move()
+	admiral.move()
 }
