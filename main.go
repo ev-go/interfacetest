97d5bf419356d5d067d8459fd11d3f5ce52f62e6
ev <dmitry_dtv@mail.ru>
2022-06-11 21:20:22 +0500

    220611
    cat type struct have strings

--- main.go
+++ main.go
@@ -2,34 +2,80 @@ package main
 
 import "fmt"
 
-type WayToMove interface {
-	move()
+type animaler interface {
+	makeSound()
+	run()
 }
 
-// структура "Автомобиль"
-type Car struct{}
-
-// структура "Самолет"
-type Aircraft struct{}
-
-type Boat struct{}
-
-func (c Car) move() {
-	fmt.Println("Автомобиль едет")
+type cat struct {
+	color string
+	class string
 }
-func (a Aircraft) move() {
-	fmt.Println("Самолет летит")
+
+type dog struct{}
+
+func (c *cat) makeSound() {
+	switch c.color {
+	case "2":
+		fmt.Println("meow")
+	case "1":
+		fmt.Println("moew")
+
+	}
+	fmt.Println("hi")
 }
-func (a Boat) move() {
-	fmt.Println("Лодка идет")
+
+func (c *cat) run() {
+	fmt.Println("Paw")
+}
+
+func (d *dog) makeSound() {
+	fmt.Println("moo")
+}
+
+func (d *dog) run() {
+	fmt.Println("PawPaw")
 }
 
 func main() {
-
-	var tesla WayToMove = Car{}
-	var boing WayToMove = Aircraft{}
-	var admiral WayToMove = Boat{}
-	tesla.move()
-	boing.move()
-	admiral.move()
+	var c, d animaler = &cat{"2", "3"}, &dog{}
+	c.makeSound()
+	d.makeSound()
+	c.run()
 }
+
+// package main
+
+// import "fmt"
+
+// type WayToMove interface {
+// 	move()
+// }
+
+// // структура "Автомобиль"
+// type Car struct{}
+
+// // структура "Самолет"
+// type Aircraft struct{}
+
+// type Boat struct{}
+
+// func (c Car) move() {
+// 	fmt.Println("Автомобиль едет")
+// }
+// func (a Aircraft) move() {
+// 	fmt.Println("Самолет летит")
+// }
+// func (a Boat) move() {
+// 	fmt.Println("Лодка идет")
+// }
+
+// func main() {
+
+// 	var tesla WayToMove = Car{}
+// 	var boing WayToMove = Aircraft{}
+// 	var admiral WayToMove = Boat{}
+// 	tesla.move()
+// 	boing.move()
+// 	admiral.move()
+// }
