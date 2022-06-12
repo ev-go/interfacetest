1d798e7db2ea8b1f60d8700c0fa65e429aafe170
ev <dmitry_dtv@mail.ru>
2022-06-12 18:38:06 +0500

    220612
    adding interfaces to xterm code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ev-go/interfacetest
+
+go 1.14
+
+require github.com/go-redis/redis/v8 v8.11.5
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
 
 type animaler interface {
 	makeSound()
@@ -10,6 +21,7 @@ type animaler interface {
 type cat struct {
 	color string
 	class string
+	speed string
 }
 
 type dog struct{}
@@ -26,7 +38,8 @@ func (c *cat) makeSound() {
 }
 
 func (c *cat) run() {
-	fmt.Println("Paw")
+	fmt.Println("Paw", c.speed)
+
 }
 
 func (d *dog) makeSound() {
@@ -37,11 +50,137 @@ func (d *dog) run() {
 	fmt.Println("PawPaw")
 }
 
+type Gettokenanswerstruct struct {
+	TokenRequestAt string
+	User           string
+	Login          string
+	Password       string
+	DataAnswer     string
+	Token          string
+}
+
+type httpRequestMessageStruct struct {
+	requestUseLogin    string
+	requestUsePassword string
+	requestUseData     string
+}
+
+type httpRequestStruct struct {
+	requestUseUrl      string
+	requestUsePort     string
+	requestUseRout     string
+	httpRequestMessage httpRequestMessageStruct
+}
+
+type httpRequester interface {
+	getToken()
+	getPrivateRout()
+}
+
+func (req *httpRequestStruct) getPrivateRout() {
+	httpRequestString := "http://localhost:3000/products"
+
+	bearer := "Bearer " //+ tokenFromRedis
+	cli := http.Client{Timeout: 5 * time.Second}
+	request, err := http.NewRequest("GET", httpRequestString, nil)
+	request.Header.Add("Authorization", bearer)
+	request.Header.Add("Content-Type", `application/json`)
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := cli.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\nresponse?:", string(responseData))
+	//return responseData
+}
+
+func (req *httpRequestStruct) getToken() {
+	httpRequestString := req.httpReqStructToString()
+	cli := http.Client{Timeout: 5 * time.Second}
+	request, err := http.NewRequest("GET", httpRequestString, nil)
+	//request.Header.Add("Authorization", bearer)
+	request.Header.Add("Content-Type", `application/json`)
+	if err != nil {
+		panic(err)
+	}
+	//defer request.Body.Close()
+
+	response, err := cli.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\nresponse?:", string(responseData))
+
+	var Gettokenanswer = &Gettokenanswerstruct{}
+	json.Unmarshal([]byte(responseData), Gettokenanswer)
+	fmt.Println("\ntoken from struct:", Gettokenanswer.Token)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	newTokenToRedis := rdb.Set(ctx, req.httpRequestMessage.requestUseLogin, Gettokenanswer.Token, 0).Err()
+	if newTokenToRedis != nil {
+		panic(newTokenToRedis)
+	}
+}
+
+func (req *httpRequestStruct) httpReqStructToString() string {
+	String := "http://" +
+		req.requestUseUrl +
+		":" +
+		req.requestUsePort +
+		"/" +
+		req.requestUseRout +
+		"?login=" +
+		req.httpRequestMessage.requestUseLogin +
+		"&password=" +
+		req.httpRequestMessage.requestUsePassword +
+		"&data=" +
+		req.httpRequestMessage.requestUseData
+
+	return String
+}
+
 func main() {
-	var c, d animaler = &cat{"2", "3"}, &dog{}
+	var c, d animaler = &cat{"2", "3", "4"}, &dog{}
 	c.makeSound()
 	d.makeSound()
 	c.run()
+
+	requestUseUrl := "localhost"
+	requestUsePort := "3000"
+	requestUseRout := "get-token"
+	requestUseLogin := "root2"
+	requestUsePassword := "1"
+	requestUseData := "21"
+
+	var httpReq httpRequester = &httpRequestStruct{
+		requestUseUrl,
+		requestUsePort,
+		requestUseRout,
+		httpRequestMessageStruct{
+			requestUseLogin,
+			requestUsePassword,
+			requestUseData}}
+	httpReq.getToken()
+	httpReq.getPrivateRout()
 }
 
 // package main
